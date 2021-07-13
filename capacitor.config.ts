b8133d9606e0cc136a8869a141d21b27885eccab
Dennis <dennis@thehumanswissarmyknife.de>
2021-07-13 18:17:02 +0200

    init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'de.dennisvocke.mokast-v2',
   appName: 'Mokast_v2',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
